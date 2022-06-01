e9554e4263590ba7d12962cdd8afef5f083db066
insaanhuvhai <104100121+insaanhuvhai@users.noreply.github.com>
2022-06-01 09:48:21 +0530

    Update creds.py

--- creds.py
+++ creds.py
@@ -6,7 +6,7 @@ load_dotenv(find_dotenv())
 
 class Credentials:
     OWNER_USERNAME = os.getenv("OWNER_USERNAME") # Your Telegram Username
-    START_PIC = os.getenv("START_PIC") # A Telegraph link to display when give /start command
+    START_PIC = os.getenv("START_PIC") # A Telegraph link of the pic to display when give /start command
     BOT_TOKEN = os.getenv("BOT_TOKEN")  # from @botfather
     API_ID = int(os.getenv("API_ID"))  # API ID from Telegram Web
     API_HASH = os.getenv("API_HASH")  # API Hash from Telegram Web
