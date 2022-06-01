23e36644317edbb166791c165e77c4946f9a631a
insaanhuvhai <104100121+insaanhuvhai@users.noreply.github.com>
2022-06-01 09:32:04 +0530

    Update creds.py

--- creds.py
+++ creds.py
@@ -5,6 +5,7 @@ load_dotenv(find_dotenv())
 
 
 class Credentials:
+    OWNER_USERNAME = os.getenv("OWNER_USERNAME") # Your Telegram Username
     BOT_TOKEN = os.getenv("BOT_TOKEN")  # from @botfather
     API_ID = int(os.getenv("API_ID"))  # from https://my.telegram.org/apps
     API_HASH = os.getenv("API_HASH")  # from https://my.telegram.org/apps
