63887463fcfc1958e1d823c4f43406de2bb1fe70
insaanhuvhai <104100121+insaanhuvhai@users.noreply.github.com>
2022-06-01 09:44:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,20 +19,21 @@ tgraph = Client(
 @tgraph.on_message(filters.command("start"))
 async def start(client, message):
     await message.reply_text(
-        text=f"Hello {message.from_user.mention},\nI'm a telegraph uploader bot to convert images to telegra.ph files.",
+        text=f"{START_PIC}
+Hello,{message.from_user.mention}, I am *Akira* Telegraph Uploader Bot which simply creates a *link* of the media sent in my PM.\n*How it works?*\nJust send me /start then provide me a image to create a link... Simple project by [Akira Dev](https://telegram.dog/Akira_News).\Maintained by {OWNER_USERNAME}.",
         disable_web_page_preview=True
     )
 
 
 @tgraph.on_message(filters.photo)
 async def getimage(client, message):
-    dwn = await message.reply_text("Downloading to my server...", True)
+    dwn = await message.reply_text("Connecting to the server....", True)
     img_path = await message.download()
-    await dwn.edit_text("Uploading as telegra.ph link...")
+    await dwn.edit_text("Uploading...")
     try:
         url_path = upload_file(img_path)[0]
     except Exception as error:
-        await dwn.edit_text(f"Oops something went wrong\n{error}")
+        await dwn.edit_text(f"Something went wrong 😐\n{error}")
         return
     await dwn.edit_text(
         text=f"<b>Link :-</b> <code>https://telegra.ph{url_path}</code>",
